97ab394e283da39dcd50b233d1232da6c239432d
Andrey <griko.and@gmail.com>
2024-04-17 09:18:00 +0200

    add field done/make migrate

--- back/rest_api/admin.py
+++ back/rest_api/admin.py
@@ -1,11 +1,14 @@
 from django.contrib import admin
 from .models import Client
 
+
 # Создаю класс для представления информации о клиентах в админке
 class ClientAdmin(admin.ModelAdmin):
     # список или кортеж со всеми полями, отображаемыми в таблице с постами
-    list_display = ('name', 'fam', 'email', 'phone', 'add_time')
-    list_filter = ('fam', 'add_time')  # фильтры по полям
+    list_display = ('name', 'fam', 'email', 'phone', 'add_time', 'done')
+    list_filter = ('fam', 'add_time', 'done')  # фильтры по полям
+    list_editable = ('done',)
+
 
 # Регистрирую модели в админке
 admin.site.register(Client, ClientAdmin)
--- back/rest_api/models.py
+++ back/rest_api/models.py
@@ -21,7 +21,7 @@ CONTINENT = [
     ('AF', 'Африка'),
     ('EU', 'Европа'),
     ('LA', 'Латинская Америка'),
-    ('AU', 'Австралазия'),
+    ('AU', 'Австралия'),
 ]
 
 # Выбор типа путешествия
@@ -42,7 +42,8 @@ class Client(models.Model):
     # otc = models.CharField(max_length=50, null=True, blank=True, verbose_name='Отчество') # не обязательное поле
     email = models.EmailField()
     phone = models.TextField(max_length=15, null=True, verbose_name='Телефон')
-    add_time = models.DateTimeField(auto_now_add=True) # время добавления записи (ставиться автоматически)
+    add_time = models.DateTimeField(auto_now_add=True)  # время добавления записи (ставиться автоматически)
+    done = models.BooleanField(default=False)
     # number = models.CharField(max_length=3, choices=NUMBER, verbose_name='Количество путешественников')
     # continent = models.CharField(max_length=2, choices=CONTINENT, verbose_name='Континент')
     # type = models.CharField(max_length=3, choices=TYPE, verbose_name='Тип путешествия')
